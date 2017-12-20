1ba7f9330f467fa46114a9c98a741945c8150043
Henrique <breim@protonmail.ch>
2017-12-20 04:08:51 -0200

    add rubocop

--- client.rb
+++ client.rb
@@ -1,25 +1,21 @@
 require 'socket'
 require_relative 'libs/encrypt.rb'
 
-sock = UDPSocket.new
-
-
 def send_msg(sock, msg)
-	data = msg
-	sock.send(data, 0, '127.0.0.1', 33333)
+  data = msg
+  sock.send(data, 0, '127.0.0.1', 33333)
 end
 
-	key = 'secret'
+sock = UDPSocket.new
 
+key = 'secret1'
 
 loop  do
 	msg = gets.chomp.to_s
-
 	cipher = msg.encrypt(key) 
-	enc   = Base64.encode64(cipher)
+	encrypted = Base64.encode64(cipher)
 
-	send_msg(sock, enc)
-	puts enc
+	send_msg(sock, encrypted)
 end
 
 sock.close
--- server.rb
+++ server.rb
@@ -1,27 +1,18 @@
 require 'socket'
 require_relative 'libs/encrypt.rb'
 
-
 BasicSocket.do_not_reverse_lookup = true
 client = UDPSocket.new
 client.bind('0.0.0.0', 33333)
 
+key = 'secret'
 
-	key = 'secret'
 loop do
 	data, addr = client.recvfrom(1024)
-
 	plain = Base64.decode64(data)
-	puts plain
-
 	msg = plain.decrypt(key)
 
-	puts msg
-	
-
+  puts msg
 end
 
 client.close
-
-
-
